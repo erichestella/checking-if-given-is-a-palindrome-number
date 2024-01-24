e00cfbd8fa1730b6fdc64643a5e285c5cb5e251b
erichestella <erichestella05@gmail.com>
2024-01-24 09:57:54 +0800

    function that defines the reversed number is palidrome

--- identifying which is palindrome numbers.py
+++ identifying which is palindrome numbers.py
@@ -11,3 +11,7 @@ define_integers = str(digital)
 #it display the reversed number
 check_num = define_integers[::-1]
 print('\nIN REVERSED: ', check_num)
+
+#function to define if the given is palidrome number
+if define_integers == check_num:
+    print('\nThe given numerical is', check_num, 'and it is defines as apalidrome number\n')
