0affd64dc3299e11217750f3e928b2a3f6d866cb
erichestella <erichestella05@gmail.com>
2024-01-24 09:16:48 +0800

    it display the number you just type

--- identifying which is palindrome numbers.py
+++ identifying which is palindrome numbers.py
@@ -1,2 +1,6 @@
 #adding up some title
 print('CHECK WHICH IS PALINDROME AND NOT IN THE GIVEN NUMBER')
+
+#it display the number you just type
+digital = int(input('\nTYPE 3 NUMBERS TO DEFINE IF ITS PALINDROME OR NOT :   '))
+print('GIVEN =', digital)
