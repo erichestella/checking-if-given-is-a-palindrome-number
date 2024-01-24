cc8dcd31f5a7f36eb667b028d72cb3a3daee1507
erichestella <erichestella05@gmail.com>
2024-01-24 09:22:53 +0800

    define the given number

--- identifying which is palindrome numbers.py
+++ identifying which is palindrome numbers.py
@@ -4,3 +4,6 @@ print('CHECK WHICH IS PALINDROME AND NOT IN THE GIVEN NUMBER')
 #it display the number you just type
 digital = int(input('\nTYPE 3 NUMBERS TO DEFINE IF ITS PALINDROME OR NOT :   '))
 print('GIVEN =', digital)
+
+#it defines the given 
+define_integers = str(digital)
