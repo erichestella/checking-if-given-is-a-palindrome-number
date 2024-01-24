cf6aa28d62a2a5956126d726afc1b116c219baa8
erichestella <erichestella05@gmail.com>
2024-01-24 12:43:35 +0800

    these functions define if the given is palindrome or not

--- identifying which is palindrome numbers.py
+++ identifying which is palindrome numbers.py
@@ -14,8 +14,8 @@ print('\nIN REVERSED: ', check_num)
 
 #function to define if the given is a palindrome number
 if define_integers == check_num:
-    print('\nThe given reversed numerical is', check_num, 'and it is defines as palindrome number\n')
+    print('\nYES. The given reversed numerical is', check_num, 'and it is defines as palindrome number\n')
 
 #function to define if the given is not a palindrome number 
 else :
-    print('\nThe given reversed numerical is', check_num, 'and it is defines as not palindrome number.\n')
+    print('\nNO. The given reversed numerical is', check_num, 'and it is defines as not palindrome number.\n')
